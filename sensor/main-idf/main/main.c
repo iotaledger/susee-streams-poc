f1b629239d44dc29083f194bf1c7a9e0ba628b4b
chrisgitiota <christof.gerritsma@iota.org>
2022-03-21 10:39:18 +0100

    Add esp-idf CMake based sensor main project similar to GH espressif/rust-esp32-example

--- sensor/main-idf/main/main.c
+++ sensor/main-idf/main/main.c
@@ -1,16 +1,9 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+#include "esp_chip_info.h"
 #include "esp_spi_flash.h"
 
 #include "RustApi.h"
@@ -35,6 +28,6 @@ void app_main(void)
 
     printf("Free heap: %d\n", esp_get_free_heap_size());
 
-    printf("Starting rust process_main function\n\n",);
-
+    printf("Starting rust process_main function\n\n");
+    process_main();
 }

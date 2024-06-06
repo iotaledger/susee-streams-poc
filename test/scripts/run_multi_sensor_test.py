a20f268511a5c3cd1cafa7067bc6acc4a8b78a6b
chrisgitiota <christof.gerritsma@iota.org>
2024-06-06 10:52:40 +0200

    Use Failover and random 50 byte messages for automatic multi Sensor tests (X86/PC)

--- test/scripts/run_multi_sensor_test.py
+++ test/scripts/run_multi_sensor_test.py
@@ -9,6 +9,7 @@ number_of_sensors = int(os.getenv('NUMBER_OF_SENSORS'))
 rust_target_folder = os.getenv('RUST_TARGET_FOLDER')
 workspace_folder = os.getenv('WORKSPACE_FOLDER')
 iota_bridge_url = os.getenv('IOTA_BRIDGE_URL')
+failover_iota_bridge = os.getenv('FAILOVER_IOTA_BRIDGE_URL')
 
 compiled_sensor_src_path = rust_target_folder + r"/sensor"
 compiled_mng_console_src_path = rust_target_folder + r"/management-console"
@@ -19,5 +20,5 @@ for indx in range(0, number_of_sensors):
     if indx < number_of_sensors - 1:
         subprocess.Popen("./sensor --file-to-send \"../../../payloads/meter_reading_1_compact.json\" --use-lorawan-rest-api --iota-bridge-url " + iota_bridge_url + " 2>>run_multi_sensor_test.log", cwd=sensor_test_folder, shell=True)
     else:
-        subprocess.run("./sensor --file-to-send \"../../../payloads/meter_reading_1_compact.json\" --use-lorawan-rest-api --iota-bridge-url " + iota_bridge_url + " 2>run_multi_sensor_test.log", cwd=sensor_test_folder, shell=True)
+        subprocess.run("./sensor --random-msg-of-size 50 --use-lorawan-rest-api --iota-bridge-url " + iota_bridge_url + " --failover-iota-bridge-url " + failover_iota_bridge + " 2>run_multi_sensor_test.log", cwd=sensor_test_folder, shell=True)
 
